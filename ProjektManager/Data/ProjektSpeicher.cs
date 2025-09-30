580c266227301582eb37bf8305cbd3ffc34080ff
mertergun95 <mertion95@gmail.com>
2025-09-30 14:51:06 +0200

    Persist projects in OneDrive directory

--- ProjektManager/Data/ProjektSpeicher.cs
+++ ProjektManager/Data/ProjektSpeicher.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using ProjektManager.Models;
 using System.IO;
+using ProjektManager.Helpers;
 
 namespace ProjektManager.Data
 {
     public static class ProjektSpeicher
     {
-        private static readonly string SpeicherPfad = "projekte.json";
+        private static string SpeicherPfad => ProjektPfadHelper.ProjekteDateiPfad;
 
         public static void Speichern(List<Projekt> projekte)
         {
+            var pfad = SpeicherPfad;
+            var verzeichnis = Path.GetDirectoryName(pfad);
+            if (!string.IsNullOrEmpty(verzeichnis))
+            {
+                Directory.CreateDirectory(verzeichnis);
+            }
+
             var json = JsonSerializer.Serialize(projekte, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(SpeicherPfad, json);
+            File.WriteAllText(pfad, json);
         }
 
         public static List<Projekt> Laden()
         {
-            if (!File.Exists(SpeicherPfad))
+            var pfad = SpeicherPfad;
+
+            if (!File.Exists(pfad))
                 return new List<Projekt>();
 
-            var json = File.ReadAllText(SpeicherPfad);
+            var json = File.ReadAllText(pfad);
             return JsonSerializer.Deserialize<List<Projekt>>(json) ?? new List<Projekt>();
         }
     }
